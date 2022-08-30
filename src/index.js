c98ffc389b3c49a1994e595102a5938ff6136ff1
Chr1sDev <christopheranderson3@me.com>
2022-08-30 02:24:18 -0500

    add event poster

--- src/index.js
+++ src/index.js
@@ -1,33 +1,49 @@
-import { getEvents } from "gcal-get-events";
-import { config } from "dotenv";
-import fs from "fs";
+import dotenv from "dotenv";
+import { Client, EmbedBuilder } from "discord.js";
+let client = new Client({ intents: "Guilds" });
+import EventsManager from "./EventsManager.js";
+dotenv.config();
 
-(async () => {
-  config();
-  const start = new Date();
-  const end = new Date();
+client.on("ready", async (client) => {
+  console.log("Logged in as " + client.user.tag);
+  setInterval(postEvent, 1000 * 60 * 60 * 24);
+});
+
+async function postEvent() {
+  let channel = client.guilds.cache
+    .get(process.env.GUILD_ID)
+    .channels.cache.get(process.env.CHANNEL_ID);
+  if (!channel) return console.log("Channel Not Found");
+
+  let start = new Date();
+  start.setDate(start.getDate());
+  let end = new Date();
   end.setDate(end.getDate() + 1);
 
-  let options = {
-    whitelist: [
-      "start",
-      "end",
-      "location",
-      "summary",
-      "description",
-      "created",
-      "updated",
-      "id",
-    ],
-    count: 5,
-    start: start,
-    end: end,
-    apiKey: process.env.API_KEY,
-    calendarId: process.env.CALENDAR_ID,
-  };
+  let events = await new EventsManager().getEvents(start, end);
+  if (events.length == 0) return console.log("No Events Today");
 
-  let res = await getEvents(options);
-  console.log(JSON.stringify(res, null, 3));
+  for (const event of events) {
+    let author = event.description.trim().split(/\((.*?)\)/);
 
-  fs.writeFileSync("events.json", JSON.stringify(res, null, 3));
-})();
+    let description = event.description;
+    description = description.substring(
+      0,
+      description.length - author[author.length - 2].length - 2
+    );
+
+    author = author[author.length - 2].substring(
+      0,
+      author[author.length - 2].length - 5
+    );
+
+    const embed = new EmbedBuilder()
+      .setColor(0xa6bde3)
+      .setAuthor({ name: event.summary })
+      .setDescription(description)
+      .setFooter({ text: "Author: " + author });
+    channel.send({ embeds: [embed] });
+  }
+}
+
+client.login(process.env.TOKEN);
