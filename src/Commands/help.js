1ef135357c6aecd7823c32050831c5e68f72262c
Chr1sDev <christopheranderson3@me.com>
2022-09-20 01:43:54 +0000

    Add help command

---
+++ src/Commands/help.js
@@ -0,0 +1,25 @@
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Command details"),
+  async execute(interaction) {
+    const helpEmbed = new EmbedBuilder().setTitle("Commands");
+
+    const commands = new Map();
+    commands.set(
+      "/start-quiz *<spotify playlist url>*",
+      "Start a music quiz in a voice channel from supplied spotify playlist *(not all songs are playable)*"
+    );
+    commands.set("/stop-quiz", "Stop the current quiz in your voice channel");
+
+    const fields = [];
+    for (let [key, value] of commands) {
+      fields.push({ name: key, value: value, inline: true });
+    }
+    helpEmbed.addFields(fields);
+
+    interaction.reply({ embeds: [helpEmbed], ephemeral: true });
+  },
+};
